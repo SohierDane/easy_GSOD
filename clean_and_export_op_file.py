5193d8f1dfade4ba13709c0f4ade0d7312d6109e
SohierDane <sohier.dane@gmail.com>
2016-08-15 23:51:49 -0700

    added function to inventory a year of a stations data

--- clean_and_export_op_file.py
+++ clean_and_export_op_file.py
@@ -13,7 +13,7 @@ from numpy import nan
 
 def reorganize_columns(df):
     """
-    Resets the columns into a useful order.
+    Reset the columns into a useful order.
     """
     header_cols = ['ID_Code', 'USAF_ID_Code', 'WBAN_ID_Code', 'Elevation', 'Country_Code',
                    'Latitude', 'Longitude', 'Date', 'Year', 'Month', 'Day',
@@ -31,7 +31,7 @@ def reorganize_columns(df):
 
 def load_op_into_dataframe(raw_f_path):
     """
-    Reads an op file into a dataframe and adds basic headers
+    Read an op file into a dataframe and adds basic headers
     """
     with open(raw_f_path, 'r') as f:
         # read one line to skip past the header
@@ -50,6 +50,9 @@ def load_op_into_dataframe(raw_f_path):
 
 
 def unpack_FRSHTT(df):
+    """
+    Extract the 6 fog/rain/etc fields into separate columns.
+    """
     col_names = ['Fog', 'Rain_or_Drizzle', 'Snow_or_Ice',
                  'Hail', 'Thunder', 'Tornado']
     for i, col_nm in enumerate(col_names):
@@ -58,7 +61,7 @@ def unpack_FRSHTT(df):
     return df
 
 
-def unpack_date_info(df):
+def unpack_date_info(df, date_col_nm='yrmoda', prefix=''):
     '''
     Unpack NASA yrmoda string in to date, year, month, day
 
@@ -66,17 +69,22 @@ def unpack_date_info(df):
     >>> extract_date_info('20020103')
     ['2002-01-03', '2002', '01', '03']
     '''
-    df['Year'] = df['yrmoda'].apply(lambda x: x[:4])
-    df['Month'] = df['yrmoda'].apply(lambda x: x[4:6])
-    df['Day'] = df['yrmoda'].apply(lambda x: x[6:])
-    df['Date'] = df['yrmoda'].apply(lambda x: '-'.join([x[:4], x[4:6], x[6:]]))
-    del df['yrmoda']
+    df[prefix+'Year'] = df[date_col_nm].apply(lambda x: x[:4])
+    df[prefix+'Month'] = df[date_col_nm].apply(lambda x: x[4:6])
+    df[prefix+'Day'] = df[date_col_nm].apply(lambda x: x[6:])
+    df[prefix+'Date'] = df[date_col_nm].apply(lambda x: '-'.join(
+        [x[:4], x[4:6], x[6:]]))
+    del df[date_col_nm]
     return df
 
 
 def unpack_quality_flags(df):
+    """
+    Extract the data quality flags from the data columns.
+    """
     for col in ['Max_Temp', 'Min_Temp']:
-        df[col+'_Quality_Flag'] = df[col].apply(lambda x: 1 if x[-1] == '*' else 0)
+        df[col+'_Quality_Flag'] = df[col].apply(
+            lambda x: 1 if x[-1] == '*' else 0)
         df[col] = df[col].apply(lambda x: str(x).rstrip('*'))
     df['Precip_Flag'] = df['Precipitation'].apply(lambda x: x[-1])
     df['Precipitation'] = df['Precipitation'].apply(lambda x: x[:-1])
@@ -85,8 +93,8 @@ def unpack_quality_flags(df):
 
 def missing_codes_to_nan(df):
     """
-    Replaces NOAA's various missing data codes with nan.
-    Treats the WBAN/USAF 99999 entries as valid.
+    Replace NOAA's various missing data codes with nan.
+    Treat the WBAN/USAF 99999 entries as valid.
     """
     columns_to_ignore = ['USAF_ID_Code', 'WBAN_ID_Code', 'ID_Code']
     columns_to_use = [x for x in df.columns if x not in columns_to_ignore]
@@ -95,7 +103,10 @@ def missing_codes_to_nan(df):
     return df
 
 
-def add_metadata(station_ID, metadata_df, lookup_field):
+def get_metadata(station_ID, metadata_df, lookup_field):
+    """
+    Find the metadata for a station, if it exists.
+    """
     if station_ID not in metadata_df.index:
         return nan
     else:
@@ -105,7 +116,7 @@ def add_metadata(station_ID, metadata_df, lookup_field):
 def raw_op_to_clean_dataframe(raw_f_path, isd_history):
     """
     Take original NASA data, drops several columns,
-    splits date into more useful formats, and converts to csv format
+    split date into more useful formats, and convert to csv format
     """
     df = load_op_into_dataframe(raw_f_path)
     df['ID_Code'] = df['USAF_ID_Code']+'-'+df['WBAN_ID_Code']
@@ -113,28 +124,56 @@ def raw_op_to_clean_dataframe(raw_f_path, isd_history):
     df = unpack_date_info(df)
     df = unpack_quality_flags(df)
     station_ID = df['ID_Code'].iloc[0]
-    df['Elevation'] = add_metadata(station_ID, isd_history, 'ELEV(M)')
-    df['Station_Name'] = add_metadata(station_ID, isd_history, 'STATION NAME')
-    df['Country_Code'] = add_metadata(station_ID, isd_history, 'CTRY')
-    df['Latitude'] = add_metadata(station_ID, isd_history, 'LAT')
-    df['Longitude'] = add_metadata(station_ID, isd_history, 'LON')
+    df['Elevation'] = get_metadata(station_ID, isd_history, 'ELEV(M)')
+    df['Station_Name'] = get_metadata(station_ID, isd_history, 'STATION NAME')
+    df['Country_Code'] = get_metadata(station_ID, isd_history, 'CTRY')
+    df['Latitude'] = get_metadata(station_ID, isd_history, 'LAT')
+    df['Longitude'] = get_metadata(station_ID, isd_history, 'LON')
     df = missing_codes_to_nan(df)
     df = reorganize_columns(df)
     return df
 
 
+def inventory_stations_year(df):
+    """"
+    Generates a dataframe with counts of populated fields by month
+    """
+    idx = '-'.join(df[["ID_Code", "Year"]].iloc[0].values.tolist())
+    col_names = ["JAN", "FEB", "MAR", "APR", "MAY",
+                 "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+    data = df.groupby('Month').count().sum(axis=1).to_frame(idx).T
+    data.columns = col_names
+    data["USAF_ID_Code"] = df["USAF_ID_Code"].iloc[0]
+    data["WBAN_ID_Code"] = df["WBAN_ID_Code"].iloc[0]
+    data["Year"] = df["Year"].iloc[0]
+    return data
+
+
 def raw_op_to_clean_csv(raw_f_path, isd_history):
     """
-    Exports .op file to a cleaned .csv
+    Export .op file to a cleaned .csv.
+
+    Return station ID as success flag.
     """
     df = raw_op_to_clean_dataframe(raw_f_path, isd_history)
     df.to_csv(index=False)
-    return None
+    return df['ID_Code'].iloc[0]
+
+
+def process_stations(stn_path_list, isd_history):
+    """
+    Unpack all op files in the list to .csv,
+    inventory the data, return set of stations found
+    """
+    
 
 
 def clean_bogus_name(station_name):
     """
-    GSOD uses 'bogus' as a keyword showing that the station name is unknown.
+    Replace station names including 'bogus' with nan.
+
+    Per the NOAA readme file, GSOD uses 'bogus' as a keyword showing
+    that the station name is unknown.
     """
     if station_name is nan:
         return nan
@@ -146,8 +185,8 @@ def clean_bogus_name(station_name):
 
 def clean_history_metadata(df):
     """
-    Replaces all unreasonable elevation, latitude, longitude values with nan.
-    Replaces all names that indicate a missing name with nan.
+    Replace all unreasonable elevation, latitude, longitude values with nan.
+    Replace all names that indicate a missing name with nan.
     Lowest real point on dry land is the border of the dead sea @ 418 M.
     https://en.wikipedia.org/wiki/Extreme_points_of_Earth#Lowest_point_.28natural.29
     """
@@ -172,6 +211,8 @@ def clean_history_metadata(df):
 
 def load_isd_history(isd_path):
     """
+    Load & clean the raw NOAA metadata file
+
     Expects the .csv version of the isd-history
     """
     metadata_df = pd.read_csv(isd_path,
@@ -179,6 +220,8 @@ def load_isd_history(isd_path):
     metadata_df['ID'] = metadata_df['USAF']+'-'+metadata_df['WBAN']
     metadata_df.set_index(['ID'])
     metadata_df = clean_history_metadata(metadata_df)
+    metadata_df = unpack_date_info(metadata_df, 'BEGIN', 'Begin_')
+    metadata_df = unpack_date_info(metadata_df, 'END', 'End_')
     return metadata_df
 
 
